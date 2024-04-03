126688bbce8e4ac37d6181a37d9c8ed0a4d32c29
kuramentooo <165797352+kuramentooo@users.noreply.github.com>
2024-04-03 14:09:21 +0200

    Add files via upload

--- TP1/appli.py
+++ TP1/appli.py
@@ -6,26 +6,7 @@ metrics = PrometheusMetrics(app)
 
 @app.route('/')
 def home():
-    return '''
-<pre>
-         _nnnn_
-        dGGGGMMb     ,"""""""""""""".
-       @p~qp~~qMb    |    coucou    |
-       M|@||@) M|   _;..............'
-       @,----.JM| -'
-      JS^\__/  qKL
-     dZP        qKRb
-    dZP          qKKb
-   fZP            SMMb
-   HZM            MMMM
-   FqM            MMMM
- __| ".        |\dS"qML
- |    `.       | `' \Zq
-_)      \.___.,|     .'
-\____   )MMMMMM|   .'
-     `-'       `--'
-</pre>
-'''
+    return "coucou"
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0')
