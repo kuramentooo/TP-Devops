02ae71cbf5d42600ba0318c085e076eb2c0f9c86
devopponent <165792058+devopponent@users.noreply.github.com>
2024-04-03 12:00:44 +0200

    Update appli.py

--- TP1/appli.py
+++ TP1/appli.py
@@ -6,7 +6,26 @@ metrics = PrometheusMetrics(app)
 
 @app.route('/')
 def home():
-    return "coucou"
+    return '''
+<pre>
+         _nnnn_
+        dGGGGMMb     ,"""""""""""""".
+       @p~qp~~qMb    |    coucou    |
+       M|@||@) M|   _;..............'
+       @,----.JM| -'
+      JS^\__/  qKL
+     dZP        qKRb
+    dZP          qKKb
+   fZP            SMMb
+   HZM            MMMM
+   FqM            MMMM
+ __| ".        |\dS"qML
+ |    `.       | `' \Zq
+_)      \.___.,|     .'
+\____   )MMMMMM|   .'
+     `-'       `--'
+</pre>
+'''
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0')
